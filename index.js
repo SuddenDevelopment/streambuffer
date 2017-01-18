6f896cf283fcd471073d733efbf5d2c84b9ac2ac
SuddenDevelopment <Anthony.Aragues@gmail.com>
2017-01-17 23:32:18 -0500

    This makes it logic complete, tomorrow=debug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //----====|| NOTES ||====----\\
-// array.splice(start, deleteCount, item1, item2, ...) returns deleted items
+// If there is new bursty data, buffer it, allow different views within it. keep caps and stuff in mind and high performance
+// If there is no new data, trickle buffered data into view, direction of trickle based on on position of the view
 //----===================----\\
 var streambuffer = function(objConfig){ 'use strict'; var self=this;
 //----====|| CONFIG ||====----\\
@@ -7,9 +8,11 @@ var streambuffer = function(objConfig){ 'use strict'; var self=this;
 		//set all defaults
 		objConfig={
 			,size:10000
-			,speed:1
+			,interval:500
 			,newest:{size:20}
 			,oldest:false
+			,fnSendRecord:function(objRecord){console.log(objRecord);}
+			,fnEndRecords:function(){console.log('no more records to stream in');}
 		};
 	}else{
 		//check config and add defaults
@@ -20,42 +23,79 @@ var streambuffer = function(objConfig){ 'use strict'; var self=this;
 //----====|| PROPERTIES ||====----\\
 	this.arrCache = arrCache;
 	this.config = objConfig;
-	this.stats = {};
+	this.stats = {total:0,current:0,last:0,count:0,avg:0,tsUpdated:0};
 	this.tsFirst = 0;
 	this.tsLast = 0;
+	this.travel=0;
 //----====|| METHODS ||====----\\
 	this.setView = function(){};
 	this.setConfig = function(){};
 	this.addData = function(arrData){
-		var intCount = arrData.length();
-		var arrViews = Object.keys(self.views);
 		//we MUST work with arrays, if it isn't stuff it into one.
 		if(arrData.constructor !== Array){ arrData=[arrData]; }
+		var intCount = arrData.length();
+		var arrViews = Object.keys(self.views);
+		self.stats.total = self.stats.total + intCount;
+		self.stats.last = self.stats.current;
+		self.stats.current = intCount;
+		self.stats.count++;
+		self.stats.avg=self.stats.total/self.stats.count;
+		self.tsLast=Date.now();
 		//if there is more than the limit, just trim and set it as the cache
-		if(intCount >= self.config.size){ arrData.splice(self.config.size); arrCache=arrData; }
+		if(intCount >= self.config.size){ arrData.splice(self.config.size); self.arrCache=arrData; }
 		//if it's one record, just do it
-		else if(intCount === 1){ arrCache.pop(); arrCache.unshift(arrData[0]); }
+		else if(intCount === 1){ arrCache.pop(); self.arrCache.unshift(arrData[0]); }
 		//otherwise need to move some chunks around
 		else{
 			//decide how many records will be kept after new ones are inserted.
 			var intKeep = self.config.size-intCount;
 			//create the new array from what's new and what's kept
-			arrCache = arrData.concat(arrCache.splice(intKeep));
+			self.arrCache = arrData.concat(self.arrCache.splice(intKeep));
 		}
 		//need to set each of the views based on their defined positions
 			//set the by position
-		if(self.newest!==false){ 
-			if(self.stats.total < self.newest.size){ self.newest.data=arrCache.slice(0,self.newest.size); }
-			else{ self.newest.data=arrCache.slice(0,self.stats.total); }
-		}
-		if(self.config.oldest!==false){
-			if(self.stats.total < self.oldest.size){ self.oldest.data=arrCache.slice(0,self.stats.total); }
-			else if(self.stats.total < self.config.size){ self.oldest.data=arrCache.slice(self.stats.total-self.oldest.size,self.oldest.size); } }
-			else{ self.oldest.data=arrCache.slice(self.config.size-self.views[arrViews[i]].size); }	 
+		fnSetViews(arrData);
+		//if this is the first set of data to be added start the streaming per interval
+		if(self.stats.total === intTotal){ 
+			self.tsFirst=Date.now();
+			fnStreamRecords(); 
 		}
 	};
-	this.reset = function(){ arrCache = new Array(self.objConfig.size); };
-	this.stop = function(){};
-	this.go = function(){};
-}
+	
+	var fnStreamRecords=function(){
+		//if there are new records this time around, no need to mess with advancing
+		var tsNow = Date,now();
+		//data hasnt been added since last interval so start advancing
+		if(tsNow > self.stats.tsUpdated+self.config.interval){ fnSetViews([]); }
+		else{ this.travel=0; }
+		self.stats.tsUpdated=Date.tsNow;
+		//call this function again at the set time	
+		if(this.config.stream===true){ setInterval(function(){ fnStreamRecords(); }, this.config.interval); }
+	};
+	var fnSetViews=function(arrData){
+		//verify there's room to advance the records
+		var intMove=0; if(arrData.length===0 && self.stats.total > self.newest.size+self.travel+1){ self.travel++; intMove=self.travel; }
+		//no more data to show, so leave buffer where they are
+		if(arrData.length===0 && intMove===0 && this.config.stream===true){ this.config.fnEndRecords(); this.config.stream=false; }
+		else{
+			if(self.newest!==false){ 
+				if(self.stats.total < self.newest.size){ self.newest.data=arrCache.slice(0,self.stats.total); }
+				else{ self.newest.data=arrCache.slice(0+intMove,self.newest.size); }
+			}
+			if(self.config.oldest!==false){
+				if(self.stats.total < self.oldest.size){ self.oldest.data=arrCache.slice(0,self.stats.total); }
+				else if(self.stats.total < self.config.size){ self.oldest.data=arrCache.slice(self.stats.total-self.oldest.size+intMove,self.oldest.size); } }
+				else{ self.oldest.data=arrCache.slice(self.config.size-self.oldest.size+intMove,self.oldest.size); }	 
+			}
+		}
+	};
+	this.Stop=function(){ this.config.stream=false; };
+	this.Go=function(){
+		//dont want to fire this unless it was stopped, lets be sure
+		if(this.config.stream===false){
+			this.config.stream=true;
+			fnStreamRecords();
+		}
+	};
+};
 if (typeof module !== 'undefined' && module.exports){module.exports = streambuffer;}
